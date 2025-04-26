36839a3fcb21c24d96f9406f20ff0f422a0dc322
Carlos Daniel <carlosdaniel.pessoal@gmail.com>
2025-04-26 20:40:17 -0300

    Joystick configured

--- logic-gates/logic-gates.c
+++ logic-gates/logic-gates.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 #include "pico/bootrom.h"
 #include "pico/stdlib.h"
+#include "pico/time.h"
 #include "hardware/i2c.h"
 #include "inc/ssd1306.h"
+#include "hardware/adc.h"
 #include "hardware/pio.h"
 #include "hardware/uart.h"
 
@@ -14,6 +16,11 @@
 #define DISPLAY_SDA_PIN 14
 #define DISPLAY_SCL_PIN 15
 #define RESET_BUTTON_PIN 22
+#define JOYSTICK_X_AXIS_PIN 26
+#define JOYSTICK_Y_AXIS_PIN 27
+
+#define JOYSTICK_X_AXIS_ADC_CHANNEL 1
+#define JOYSTICK_Y_AXIS_ADC_CHANNEL 0
 
 struct render_area frame_area = {
     start_column : 0,
@@ -22,18 +29,108 @@ struct render_area frame_area = {
     end_page : ssd1306_n_pages - 1
 };
 
+unsigned short int joystickXValue = 0;
+unsigned short int joystickYValue = 0;
+
+unsigned int previousLogicGate = 1;
+unsigned int currentLogicGate = 1;
+
+void wait(unsigned long int milliseconds);
 void initializeComponents();
 void setDisplay(char *message);
+void readJoystickValue();
 
 int main()
 {
     initializeComponents();
+
+    setDisplay("AND");
     
     while (true) {
+        if (currentLogicGate != previousLogicGate)
+        {
+            switch (currentLogicGate)
+            {
+            case 1:
+            setDisplay("AND");
+                break;
+
+            case 2:
+            setDisplay("OR");
+                break;
+                
+            case 3:
+            setDisplay("NOT");
+                break;
+                
+            case 4:
+            setDisplay("NAND");
+                break;
+
+            case 5:
+            setDisplay("NOR");
+                break;
+
+            case 6:
+            setDisplay("XOR");
+                break;
+
+            case 7:
+            setDisplay("XNOR");
+                break;
+                
+            default:
+            setDisplay("AND");
+                break;
+            }
+
+            previousLogicGate = currentLogicGate;
+        }
+
         if (gpio_get(RESET_BUTTON_PIN) == PRESSED)
         {
             reset_usb_boot(0, 0);
         }
+        else
+        {
+            readJoystickValue();
+            
+            if (joystickXValue > 3584)
+            {
+                if (currentLogicGate == 7)
+                {
+                    currentLogicGate = 1;
+                }
+                else
+                {
+                    currentLogicGate++;
+                }
+            }
+            else if (joystickXValue < 512)
+            {
+                if (currentLogicGate == 1)
+                {
+                    currentLogicGate = 7;
+                }
+                else
+                {
+                    currentLogicGate--;
+                }
+            }
+            
+            wait(150);
+        }
+    }
+}
+
+void wait(unsigned long int milliseconds)
+{
+    unsigned long int previousTime = to_ms_since_boot(get_absolute_time());
+    unsigned long int currentTime = previousTime;
+
+    while (currentTime - previousTime <= milliseconds)
+    {
+        currentTime = to_ms_since_boot(get_absolute_time());
     }
 }
 
@@ -53,6 +150,10 @@ void initializeComponents()
     ssd1306_init();
     calculate_render_area_buffer_length(&frame_area);
     setDisplay("Inicializando");
+    
+    adc_init();
+    adc_gpio_init(JOYSTICK_X_AXIS_PIN);
+    adc_gpio_init(JOYSTICK_Y_AXIS_PIN);
 }
 
 void setDisplay(char *message)
@@ -63,4 +164,15 @@ void setDisplay(char *message)
     ssd1306_draw_string(ssd, 0, 8, message);
     
     render_on_display(ssd, &frame_area);
+}
+
+void readJoystickValue()
+{
+    adc_select_input(JOYSTICK_X_AXIS_ADC_CHANNEL);
+    sleep_us(2);
+    joystickXValue = adc_read();
+    
+    adc_select_input(JOYSTICK_Y_AXIS_ADC_CHANNEL);
+    sleep_us(2);
+    joystickYValue = adc_read();
 }
