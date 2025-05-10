477c0b941ac0d25f58282aeff659d8af66e849a5
Carlos Daniel <carlosdaniel.pessoal@gmail.com>
2025-05-10 16:16:24 -0300

    LED RGB configured

--- automatic-turnstile/automatic-turnstile.c
+++ automatic-turnstile/automatic-turnstile.c
@@ -6,6 +6,7 @@
 #include "hardware/i2c.h"
 #include "inc/ssd1306.h"
 #include "hardware/adc.h"
+#include "hardware/pwm.h"
 #include "hardware/pio.h"
 #include "hardware/uart.h"
 
@@ -13,6 +14,8 @@
 #define OUT 1
 #define PRESSED 0
 #define NOT_PRESSED 1
+#define OFF 0
+#define ON 1024
 
 #define NONE 0
 #define UP 1
@@ -21,6 +24,9 @@
 #define LEFT 4
 
 #define RESET_BUTTON_PIN 6
+#define GREEN_LED_PIN 11
+#define BLUE_LED_PIN 12
+#define RED_LED_PIN 13
 #define DISPLAY_SDA_PIN 14
 #define DISPLAY_SCL_PIN 15
 #define JOYSTICK_BUTTON_PIN 22
@@ -29,6 +35,14 @@
 
 #define JOYSTICK_X_AXIS_ADC_CHANNEL 1
 #define JOYSTICK_Y_AXIS_ADC_CHANNEL 0
+
+#define PWM_DIVIDER 16
+#define PWM_PERIOD 4096
+
+#define TURN_OFF 0
+#define RED 1
+#define GREEN 2
+#define BLUE 3
 
 #define GR 0
 #define HO 1
@@ -47,6 +61,9 @@ bool joystickButtonStatus = NOT_PRESSED;
 
 unsigned char joystickDirection = NONE;
 
+unsigned short int redLedPwmLevel = OFF, greenLedPwmLevel = OFF, blueLedPwmLevel = OFF;
+unsigned int redLedSlice, greenLedSlice, blueLedSlice;
+
 unsigned char previousEntry = PT;
 unsigned char currentEntry = GR;
 
@@ -62,6 +79,7 @@ void readButtons();
 void setDisplay(char *message);
 void updateDisplay();
 void readJoystickDirection();
+void setLED(char color);
 
 int main()
 {
@@ -143,6 +161,26 @@ void initializeComponents()
     adc_init();
     adc_gpio_init(JOYSTICK_X_AXIS_PIN);
     adc_gpio_init(JOYSTICK_Y_AXIS_PIN);
+    
+    gpio_set_function(RED_LED_PIN, GPIO_FUNC_PWM);
+    redLedSlice = pwm_gpio_to_slice_num(RED_LED_PIN);
+    pwm_set_clkdiv(redLedSlice, PWM_DIVIDER);
+    pwm_set_wrap(redLedSlice, PWM_PERIOD);
+    pwm_set_gpio_level(RED_LED_PIN, redLedPwmLevel);
+    pwm_set_enabled(redLedSlice, true);
+    gpio_set_function(GREEN_LED_PIN, GPIO_FUNC_PWM);
+    greenLedSlice = pwm_gpio_to_slice_num(GREEN_LED_PIN);
+    pwm_set_clkdiv(greenLedSlice, PWM_DIVIDER);
+    pwm_set_wrap(greenLedSlice, PWM_PERIOD);
+    pwm_set_gpio_level(GREEN_LED_PIN, greenLedPwmLevel);
+    pwm_set_enabled(greenLedSlice, true);
+    gpio_set_function(BLUE_LED_PIN, GPIO_FUNC_PWM);
+    blueLedSlice = pwm_gpio_to_slice_num(BLUE_LED_PIN);
+    pwm_set_clkdiv(blueLedSlice, PWM_DIVIDER);
+    pwm_set_wrap(blueLedSlice, PWM_PERIOD);
+    pwm_set_gpio_level(BLUE_LED_PIN, blueLedPwmLevel);
+    pwm_set_enabled(blueLedSlice, true);
+    setLED(TURN_OFF);
 }
 
 void readButtons()
@@ -224,5 +262,41 @@ void readJoystickDirection()
     else
     {
         joystickDirection = NONE;
+    }
+}
+
+void setLED(char color)
+{
+    switch (color)
+    {
+    case TURN_OFF:
+    pwm_set_gpio_level(RED_LED_PIN, OFF);
+    pwm_set_gpio_level(GREEN_LED_PIN, OFF);
+    pwm_set_gpio_level(BLUE_LED_PIN, OFF);
+    break;
+
+    case RED:
+    pwm_set_gpio_level(RED_LED_PIN, ON);
+    pwm_set_gpio_level(GREEN_LED_PIN, OFF);
+    pwm_set_gpio_level(BLUE_LED_PIN, OFF);
+    break;
+
+    case GREEN:
+    pwm_set_gpio_level(RED_LED_PIN, OFF);
+    pwm_set_gpio_level(GREEN_LED_PIN, ON);
+    pwm_set_gpio_level(BLUE_LED_PIN, OFF);
+    break;
+
+    case BLUE:
+    pwm_set_gpio_level(RED_LED_PIN, OFF);
+    pwm_set_gpio_level(GREEN_LED_PIN, OFF);
+    pwm_set_gpio_level(BLUE_LED_PIN, ON);
+    break;
+
+    default:
+    pwm_set_gpio_level(RED_LED_PIN, OFF);
+    pwm_set_gpio_level(GREEN_LED_PIN, OFF);
+    pwm_set_gpio_level(BLUE_LED_PIN, OFF);
+    break;
     }
 }
